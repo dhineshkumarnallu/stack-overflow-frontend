57bbd21b9d0526e7f799cd7701dfcc7b1789a51c
dhineshkumarnallu <dhinesh21072001@gmail.com>
2023-07-17 22:04:39 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import { BrowserRouter as Router } from 'react-router-dom';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import './App.css';
+import Navbar from './Components/Navbar/Navbar';
+import { fetchAllQuestions } from './actions/question';
+import Root from './Root';
+import { fetchAllUsers } from './actions/users';
+
+
+
+function App() {
+  const dispatch = useDispatch()
+
+  useEffect(()=>{
+    dispatch(fetchAllQuestions())
+    dispatch(fetchAllUsers())
+  },[dispatch])
+
+  return (
+  <div className="App">
+    <Router>
+      <Navbar/>
+      <Root/>
+    </Router>     
+  </div>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import { Provider } from 'react-redux';
+import { applyMiddleware, compose} from 'redux'
+import { configureStore } from '@reduxjs/toolkit';
+import thunk from 'redux-thunk';
+import reducer from './reducers'
+
+
+const store = configureStore({reducer},compose(applyMiddleware(thunk)))
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <Provider store={store}>
+     <React.StrictMode>
+    <App />
+  </React.StrictMode>b
+  </Provider>
+ 
+);
+
